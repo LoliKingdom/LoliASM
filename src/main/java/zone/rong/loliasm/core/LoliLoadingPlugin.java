4db7f698a00061b3b556c2858a5859634a18a4b7
Rongmario <rongmario@protonmail.com>
2022-04-23 13:40:38 +0100

    Bump to 4.10.2

--- src/main/java/zone/rong/loliasm/core/LoliLoadingPlugin.java
+++ src/main/java/zone/rong/loliasm/core/LoliLoadingPlugin.java
@@ -27,7 +27,7 @@ import java.util.zip.ZipFile;
 @IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
 public class LoliLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
-    public static final String VERSION = "4.10.1";
+    public static final String VERSION = "4.10.2";
 
     public static final boolean isDeobf = FMLLaunchHandler.isDeobfuscatedEnvironment();
 
