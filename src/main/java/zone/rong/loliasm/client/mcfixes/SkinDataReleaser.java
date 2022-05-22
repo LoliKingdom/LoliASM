c4e428def92a9ff5eef08b5f0328e427b4ac424c
Rongmario <rongmario@protonmail.com>
2022-05-22 01:13:10 +0100

    Queue work on client thread

--- src/main/java/zone/rong/loliasm/client/mcfixes/SkinDataReleaser.java
+++ src/main/java/zone/rong/loliasm/client/mcfixes/SkinDataReleaser.java
@@ -17,23 +17,25 @@ public class SkinDataReleaser {
 
     @SubscribeEvent
     public static void onClientDisconnect(FMLNetworkEvent.ClientDisconnectionFromServerEvent event) {
-        Map<ResourceLocation, ITextureObject> textureObjects = ((TextureManagerAccessor) Minecraft.getMinecraft().getTextureManager()).getMapTextureObjects();
-        if (textureObjects != null) {
-            int count = 0;
-            Iterator<Entry<ResourceLocation, ITextureObject>> entryIter = textureObjects.entrySet().iterator();
-            while (entryIter.hasNext()) {
-                Entry<ResourceLocation, ITextureObject> entry = entryIter.next();
-                if (entry.getValue() instanceof ThreadDownloadImageData) {
-                    if (entry.getKey().getPath().startsWith("skins/")) {
-                        ((ThreadDownloadImageData) entry.getValue()).deleteGlTexture();
-                        entryIter.remove();
-                        LoliLogger.instance.debug("Released {} texture", entry.getKey());
-                        count++;
+        Minecraft.getMinecraft().addScheduledTask(() -> {
+            Map<ResourceLocation, ITextureObject> textureObjects = ((TextureManagerAccessor) Minecraft.getMinecraft().getTextureManager()).getMapTextureObjects();
+            if (textureObjects != null) {
+                int count = 0;
+                Iterator<Entry<ResourceLocation, ITextureObject>> entryIter = textureObjects.entrySet().iterator();
+                while (entryIter.hasNext()) {
+                    Entry<ResourceLocation, ITextureObject> entry = entryIter.next();
+                    if (entry.getValue() instanceof ThreadDownloadImageData) {
+                        if (entry.getKey().getPath().startsWith("skins/")) {
+                            ((ThreadDownloadImageData) entry.getValue()).deleteGlTexture();
+                            entryIter.remove();
+                            LoliLogger.instance.debug("Released {} texture", entry.getKey());
+                            count++;
+                        }
                     }
                 }
+                LoliLogger.instance.info("Released {} skin textures", count);
             }
-            LoliLogger.instance.info("Released {} skin textures", count);
-        }
+        });
     }
 
 }
