2fc06905b251d12b667cb96e46027abcbe3f7112
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-20 20:25:11 -0400

    Revamp releaseSpriteFramesCache
    Now loads/unloads frame data dynamically for ALL sprites
    Data not referenced for 20 consecutive client ticks is unloaded

--- src/main/java/zone/rong/loliasm/common/internal/mixins/TextureMapAccessor.java
+++ src/main/java/zone/rong/loliasm/common/internal/mixins/TextureMapAccessor.java
@@ -1,2 +1,14 @@
-package zone.rong.loliasm.common.internal.mixins;public interface TextureMapAccessor {
+package zone.rong.loliasm.common.internal.mixins;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(TextureMap.class)
+public interface TextureMapAccessor {
+    @Accessor("mapUploadedSprites")
+    Map<String, TextureAtlasSprite> getMapUploadedSprites();
 }
--- src/main/java/zone/rong/loliasm/proxy/ClientProxy.java
+++ src/main/java/zone/rong/loliasm/proxy/ClientProxy.java
@@ -1,8 +1,6 @@
 package zone.rong.loliasm.proxy;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.IReloadableResourceManager;
 import net.minecraftforge.client.resource.ISelectiveResourceReloadListener;
 import net.minecraftforge.client.resource.VanillaResourceType;
@@ -13,8 +11,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
-import pl.asie.foamfix.shared.FoamFixShared;
-import slimeknights.tconstruct.library.client.texture.AbstractColoredTexture;
 import zone.rong.loliasm.LoliLogger;
 import zone.rong.loliasm.LoliReflector;
 import zone.rong.loliasm.bakedquad.LoliVertexDataPool;
@@ -33,19 +29,6 @@ import java.util.Map;
 public class ClientProxy extends CommonProxy {
 
     public static final List<Runnable> refreshAfterModels = new ArrayList<>();
-    public static final boolean flushTinkerSpriteFrameTextureData;
-
-    static {
-        boolean static$flushTinkerSpriteFrameTextureData = true;
-        if (Loader.isModLoaded("tconstruct") && Loader.isModLoaded("foamfix")) {
-            if (FoamFixShared.config.clDynamicItemModels) {
-                static$flushTinkerSpriteFrameTextureData = false;
-            }
-        }
-        flushTinkerSpriteFrameTextureData = static$flushTinkerSpriteFrameTextureData;
-    }
-
-    public static boolean canReload = true;
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
@@ -90,22 +73,6 @@ public class ClientProxy extends CommonProxy {
                     LoliBakedDynBucket.flippedBaseQuads.clear();
                     LoliBakedDynBucket.coverQuads.clear();
                     LoliBakedDynBucket.flippedCoverQuads.clear();
-                }
-                if (LoliConfig.instance.releaseSpriteFramesCache) {
-                    canReload = false;
-                    try {
-                        for (TextureAtlasSprite sprite : ((Map<String, TextureAtlasSprite>) LoliReflector.resolveFieldGetter(TextureMap.class, "mapRegisteredSprites", "field_110574_e").invoke(Minecraft.getMinecraft().getTextureMapBlocks())).values()) {
-                            if (!sprite.hasAnimationMetadata()) {
-                                if (!flushTinkerSpriteFrameTextureData && sprite instanceof AbstractColoredTexture) {
-                                    continue;
-                                }
-                                sprite.setFramesTextureData(new FramesTextureData(sprite));
-                            }
-                        }
-                    } catch (Throwable e) {
-                        e.printStackTrace();
-                    }
-                    canReload = true;
                 }
                 if (!LoliTransformer.isOptifineInstalled && LoliConfig.instance.vertexDataCanonicalization) {
                     LoliVertexDataPool.invalidate();
