36ed50ee72e2b5215cd860051fe8208b937b93e4
Rongmario <rongmario@protonmail.com>
2023-09-24 00:18:50 +0100

    Fixes #189

--- src/main/java/zone/rong/loliasm/common/crashes/mixins/EntityMixin.java
+++ src/main/java/zone/rong/loliasm/common/crashes/mixins/EntityMixin.java
@@ -7,13 +7,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import zone.rong.loliasm.common.crashes.CrashUtils;
 
 @Mixin(Entity.class)
 public class EntityMixin {
 
     @Inject(method = "addEntityCrashInfo", at = @At("TAIL"))
     private void onAddEntityCrashInfo(CrashReportCategory category, CallbackInfo ci) {
-        category.addDetail("Entity NBT", () -> ((Entity) (Object) this).writeToNBT(new NBTTagCompound()).toString());
+        if (!CrashUtils.WRITING_DETAIL.get()) {
+            CrashUtils.WRITING_DETAIL.set(true);
+            category.addDetail("Entity NBT", () -> ((Entity) (Object) this).writeToNBT(new NBTTagCompound()).toString());
+            CrashUtils.WRITING_DETAIL.set(false);
+        }
     }
 
 }
--- src/main/java/zone/rong/loliasm/common/crashes/mixins/TileEntityMixin.java
+++ src/main/java/zone/rong/loliasm/common/crashes/mixins/TileEntityMixin.java
@@ -7,14 +7,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import zone.rong.loliasm.common.crashes.CrashUtils;
 
 @Mixin(TileEntity.class)
 public class TileEntityMixin {
 
-    // "Block Entity" to stay consistent with vanilla strings
     @Inject(method = "addInfoToCrashReport", at = @At("TAIL"))
     private void onAddEntityCrashInfo(CrashReportCategory category, CallbackInfo ci) {
-        category.addDetail("Block Entity NBT", () -> ((TileEntity) (Object) this).writeToNBT(new NBTTagCompound()).toString());
+        if (!CrashUtils.WRITING_DETAIL.get()) {
+            CrashUtils.WRITING_DETAIL.set(true);
+            category.addDetail("NBT", () -> ((TileEntity) (Object) this).writeToNBT(new NBTTagCompound()).toString());
+            CrashUtils.WRITING_DETAIL.set(false);
+        }
     }
 
 }
