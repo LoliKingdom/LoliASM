4c56e46cd893006de1300687df47c9cec6db7e70
unnecessarymb <143959812+unnecessarymb@users.noreply.github.com>
2024-01-27 21:51:42 -0500

    Bump version.

--- src/main/java/zone/rong/blahajasm/core/BlahajLoadingPlugin.java
+++ src/main/java/zone/rong/blahajasm/core/BlahajLoadingPlugin.java
@@ -31,7 +31,7 @@ import java.util.zip.ZipFile;
 @IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
 public class BlahajLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
-    public static final String VERSION = "5.18";
+    public static final String VERSION = "6.0";
 
     public static final boolean isDeobf = FMLLaunchHandler.isDeobfuscatedEnvironment();
 
