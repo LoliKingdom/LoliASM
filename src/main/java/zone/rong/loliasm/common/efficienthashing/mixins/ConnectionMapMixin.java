452720f6e2e01b718d1856136fe29580258a6a5d
Rongmario <rongmario@proton.me>
2025-03-14 11:14:06 +0000

    Fixes a 3 year long bug pertaining BlockPos hashing and Railcraft
    - Thanks to @3TUSK

---
+++ src/main/java/zone/rong/loliasm/common/efficienthashing/mixins/ConnectionMapMixin.java
@@ -0,0 +1,24 @@
+package zone.rong.loliasm.common.efficienthashing.mixins;
+
+import com.google.common.collect.ForwardingMap;
+import mods.railcraft.api.charge.IChargeBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+
+import java.util.EnumSet;
+
+@Pseudo
+@Mixin(targets = "mods.railcraft.common.util.charge.ChargeNetwork$ConnectionMap", remap = false)
+public abstract class ConnectionMapMixin extends ForwardingMap<Vec3i, EnumSet<IChargeBlock.ConnectType>> {
+
+    @Override
+    public EnumSet<IChargeBlock.ConnectType> put(Vec3i key, EnumSet<IChargeBlock.ConnectType> value) {
+        if (key.getClass() == Vec3i.class) {
+            key = new BlockPos(key.getX(), key.getY(), key.getZ());
+        }
+        return super.put(key, value);
+    }
+
+}
